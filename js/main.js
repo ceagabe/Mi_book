861dd4d9054191b1db9c9b4f1cfb3fba46d50253
ceagabe <gabriel_benedetti@hotmail.com>
2023-10-06 12:32:21 -0300

    agregue main.js y definir el header y footer por js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
+ cad = `
+<div class="header-logo"> <a href="index.html"><img src="./imagenes/OIP (8).jpg" alt=""></a>
+<h1>
+    My book online
+</h1>
 
-let cad=` <h4>Redes Sociales</h4>
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<div class="container-fluid">
+    <a class="navbar-brand" href="index.html">My book </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.html" role="button">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="series.html" role="button">Series</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="novedades.html" role="button">Novedades</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="registro.html" role="button">Registro</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contactos.html" role="button">Contactos</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+</nav> ` 
+document.querySelector("header").innerHTML=cad
+
+let cad2=` <h4>Redes Sociales</h4>
 <nav class="footer"> <!-- redes sociales-->
 
     <a href="https://www.facebook.com/">
@@ -14,7 +52,6 @@ let cad=` <h4>Redes Sociales</h4>
         <img id="tel" class=navimagen src="./imagenes/whatsapp.png" alt=""> </a>
 </nav>
 
-<p> &copy; Desarrollado por GRUPO tienda on line  </p> `
+<p> &copy; Desarrollado por GRUPO Tienda Online  </p> `
 
-
-document.querySelector("footer").innerHTML=cad
+document.querySelector("footer").innerHTML=cad2
