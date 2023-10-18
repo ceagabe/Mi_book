7aa63981046f353551b1069a70282726dad0019d
ceagabe <gabriel_benedetti@hotmail.com>
2023-10-18 15:22:02 -0300

    modifique y agregue la hoja Libro_api, api.js

--- js/api.js
+++ js/api.js
@@ -3,23 +3,23 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            url:"https://www.googleapis.com/books/v1/volumes?q=search+terms/",
+            url: "https://www.googleapis.com/books/v1/volumes?q=search+terms/",
             error: false,
             datos: []
         }
     },
 
-    methods:{
-    fetchData(url) {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                this.datos = data
-            });
-    }
-},
-    created(){
-          this.fetchData(this.url)
-},
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.datos = data
+                });
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
 }).mount('#app')
