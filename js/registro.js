4b17b35c012bde3410d2035f31ad7877c49cb564
Keyla Diaz <keyladiazv2712@gmail.com>
2023-10-21 23:52:19 -0300

    validacion de formulario

--- js/registro.js
+++ js/registro.js
@@ -20,3 +20,39 @@ signUp.onclick = function () {
     signIn.classList.add("disable");
 
 }
+
+const userNameField = document.querySelector("[placeholder=Nombre]");
+const emailField = document.querySelector("[placeholder=Correo]");
+const passwordField = document.querySelector("[placeholder=Contraseña]");
+
+const validateEmptyField = (message, e) => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    if (fieldValue.trim().length <= 0) {
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = message;
+    } 
+    else {
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+
+const validateEmailFormat = e => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    if (fieldValue.trim().length > 9 && !regex.test(fieldValue)) {
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "Por favor ingrese un correo válido";
+    } else {
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+
+userNameField.addEventListener("blur", (e) => validateEmptyField ("Por favor ingrese su Nombre", e));
+emailField.addEventListener("blur", (e) => validateEmptyField ("Por favor ingrese su Correo", e));
+passwordField.addEventListener("blur", (e) => validateEmptyField ("Por favor ingrese su Contraseña", e));
+
+emailField.addEventListener("input", validateEmailFormat)
